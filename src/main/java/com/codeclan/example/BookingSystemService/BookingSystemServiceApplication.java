9ecc3b91aa1e353da6e3837c4527d6da1dc78cdc
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-08 11:50:29 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/BookingSystemService/BookingSystemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.BookingSystemService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/BookingSystemService/BookingSystemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.BookingSystemService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
