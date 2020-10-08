e848e4bfd34df9311591a4518dd6ccae2883299c
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-08 12:15:10 +0100

    completes booking model

--- src/main/java/com/codeclan/example/BookingSystemService/models/Booking.java
+++ src/main/java/com/codeclan/example/BookingSystemService/models/Booking.java
@@ -28,8 +28,10 @@ public class Booking {
     public Booking() {
     }
 
-    public Booking(String date) {
+    public Booking(String date, Course course, Customer customer) {
         this.date = date;
+        this.course = course;
+        this.customer = customer;
     }
 
     public Long getId() {
@@ -46,5 +48,21 @@ public class Booking {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 }
