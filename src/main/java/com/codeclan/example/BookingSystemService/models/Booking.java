36ccd70f0a121c5521fe17d53e7220fdfd5f83ca
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-08 12:14:25 +0100

    adds properties and getters/setters for models

--- src/main/java/com/codeclan/example/BookingSystemService/models/Booking.java
+++ src/main/java/com/codeclan/example/BookingSystemService/models/Booking.java
@@ -1,4 +1,50 @@
 package com.codeclan.example.BookingSystemService.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private String date;
+
+    @JsonIgnoreProperties(value = "bookings")
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @JsonIgnoreProperties(value = "bookings")
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public Booking() {
+    }
+
+    public Booking(String date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/codeclan/example/BookingSystemService/models/Course.java
+++ src/main/java/com/codeclan/example/BookingSystemService/models/Course.java
@@ -1,4 +1,83 @@
 package com.codeclan.example.BookingSystemService.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String town;
+
+    @Column
+    private int starRating;
+
+    @JsonIgnoreProperties(value = "course")
+    @OneToMany(mappedBy = "course")
+    private List<Booking> bookings;
+
+    public Course() {
+    }
+
+    public Course(String name, String town, int starRating, List<Booking> bookings) {
+        this.name = name;
+        this.town = town;
+        this.starRating = starRating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
 }
