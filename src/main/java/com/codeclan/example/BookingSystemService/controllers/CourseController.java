66c8bd5a069868a30c80b8a1c5797a305366cb96
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-08 14:52:11 +0100

    adds routes for course and customer controllers

--- src/main/java/com/codeclan/example/BookingSystemService/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/BookingSystemService/controllers/CourseController.java
@@ -1,9 +1,34 @@
 package com.codeclan.example.BookingSystemService.controllers;
 
+import com.codeclan.example.BookingSystemService.models.Course;
+import com.codeclan.example.BookingSystemService.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CourseController {
 
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping("/courses")
+    public ResponseEntity<List<Course>> getAllCoursesAndFilters(
+            @RequestParam(name = "rating", required = false) Integer rating,
+            @RequestParam(name = "customer", required = false) Long customerId
+    ){
+        if(rating != null){
+            return new ResponseEntity<>(courseRepository.findCourseByStarRating(rating), HttpStatus.OK);
+        }
+        if(customerId != null){
+            return new ResponseEntity<>(courseRepository.findByBookingsCustomerId(customerId), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/codeclan/example/BookingSystemService/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/BookingSystemService/controllers/CustomerController.java
@@ -1,7 +1,30 @@
 package com.codeclan.example.BookingSystemService.controllers;
 
+import com.codeclan.example.BookingSystemService.models.Customer;
+import com.codeclan.example.BookingSystemService.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers(
+            @RequestParam(name="course", required = false) Long id
+    ){
+        if (id != null){
+            return new ResponseEntity<>(customerRepository.findCustomersByBookingsCourseId(id), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+        }
+    }
 }
